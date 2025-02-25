f0dab207e4c50d20c40e126ad9c63bf55bc5a8a3
kyle-black <kpblack87@gmail.com>
2025-02-25 10:29:14 -0600

    bones of oop

---
+++ src/Trade_Sequence_oop.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as pn
+import live_data_pull
+import model_run
+import process_data
+import Buy_Sell
+
+
+
+class TradeProcess:
+    def __init__(self) -> None:
+        pass
+
+
+
+    def pull_forex_prices(self):
+        pass
+
+    def data_processes(self):
+        pass
+
+    def make_predictions(self):
+        pass
+
+    def live_trade(self):
+        pass
+
+    
